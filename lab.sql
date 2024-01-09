edc7eda336b589b43f943f57106366215f553452
2022908061 <147015307+2022908061@users.noreply.github.com>
2024-01-09 15:07:36 +0800

    Add files via upload

---
+++ lab.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `crud_new` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `crud_new` (`id`, `name`, `email`, `phone`, `image`) VALUES
+('0', 'Chelsea Evva Bernard', 'chelsea@gmail.com', '0132509874', 'chelsea.jpg'),
+('1', 'Nurina Afiqah', 'nurina@gmail.com', '0192512016', 'nurina.jpg'),
+('2', 'Nurul Qistina', 'qis@gmail.com', '01137711734', 'qis.jpg'),
+('3', 'Sharifah Najwa', 'najwa@gmail.com', '0124449183', 'najwa.jpg');
